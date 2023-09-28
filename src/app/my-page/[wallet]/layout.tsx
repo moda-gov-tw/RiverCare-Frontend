1fb654c271094f9976954a9bd93005381d04f7cb
DennisSung <dennissung@awespire.tech>
2023-09-28 14:57:46 +0800

    feat: add mypage

--- src/app/my-page/[wallet]/layout.tsx
+++ src/app/my-page/[wallet]/layout.tsx
@@ -1,9 +1,9 @@
 import Dropdown from "@/components/dropdown"
 
-export default function RiverLayout({ children }: { children: React.ReactNode }) {
+export default function MypageLayout({ children }: { children: React.ReactNode }) {
   return (
     <section className="w-full">
-      <div className="text-2xl text-title">Rivercare</div>
+      <div className="text-2xl text-title">My Page</div>
       <Dropdown />
       {/* <nav className="z-60 fixed left-0 border">123</nav> */}
       {children}
---
+++ src/app/my-page/[wallet]/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import EventToken from "@/components/token/event-token"
+import StewardshipToken from "@/components/token/stewardship-token"
+
+export default function Mypage({ params }: { params: { wallet: string } }) {
+  return (
+    <main className="m-4 w-auto border border-gray text-left">
+      <div className="m-6 text-xl font-bold">River Title</div>
+      <StewardshipToken />
+      <EventToken />
+    </main>
+  )
+}
