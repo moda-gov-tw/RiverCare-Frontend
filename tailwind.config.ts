22371e90a443e49f67b7e7b2030dc4741ea91d3c
DennisSung <dennissung@awespire.tech>
2023-09-28 12:10:05 +0800

    feat: setup global style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      colors: {
+        primary: "var(--primary-color)",
+        action: "var(--action-color)",
+        highlight: "var(--highlight-color)",
+        danger: "var(--danger-color)",
+        title: "var(--title-color)",
+        white: "var(--white)",
+        gray: "var(--gray)",
+        black: "var(--black)"
       }
     }
   },
