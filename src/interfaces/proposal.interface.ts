89475d03728fb9637e765ac9c6668381a4f1b8a6
DennisSung <dennissung@awespire.tech>
2023-10-06 14:25:57 +0800

    feat: add interface

---
+++ src/interfaces/proposal.interface.ts
@@ -0,0 +1,26 @@
+export enum ProposalStatus {
+  dead,
+  proposing,
+  approved
+}
+
+export enum ProposalType {
+  transferTezos,
+  updateAgreement,
+  updatePrompt
+}
+
+export declare interface Proposal {
+  uid: string // riverId-proposalId
+  transactionType: ProposalType
+  agreement: string // only updateAgreement
+  prompt: string[] // only updatePrompt
+  targetAddr: string // only transfer
+  transferMutez: number // only transfer
+  proposerAddr: string
+  status: ProposalStatus
+  createdTime: string
+  expiredTime: string
+  approvals: string[]
+  approvalCount: number
+}
