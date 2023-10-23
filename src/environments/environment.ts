6c2f2db395f056c9641cf3d158ec35073c30c61d
NakoOn/Awespire <akaswap.com@gmail.com>
2023-10-23 15:26:41 +0800

    feat: change rpc env to ghostnet

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 import { NetworkType } from "@airgap/beacon-types"
 
-export const NETWORK_TYPE = NetworkType.CUSTOM
-export const NODE_URL = "https://dev.akaswap.com:8888/tezos"
+export const NETWORK_TYPE = NetworkType.GHOSTNET
+export const NODE_URL = "https://ghostnet.tezos.marigold.dev/"
