babda959f1be301ebb69638f38ee50048785251d
DennisSung <dennissung@awespire.tech>
2023-12-12 12:00:24 +0800

    fix: stewardship token generation display

--- src/components/token/stewardship-token.tsx
+++ src/components/token/stewardship-token.tsx
@@ -8,19 +8,22 @@ import useSWR from "swr"
 const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
 const StewardshipToken = ({ river }: { river: any }) => {
+  let riverDetail: River | null = null
+
   const { data } = useSWR(`${BASE_URL}/api/rivers/${river.riverId}`, fetcher)
   if (data !== undefined && !data?.error) {
-    river = data
+    riverDetail = data
   }
 
-  return (
-    <div className="m-8 border bg-white p-2 text-left">
-      <Link href={`/river/${river.id}`}>
-        <Image className="mx-auto" src={StewardshipTokenSrc} alt="" width={200} height={200} />
-        <div className="mx-2 mb-1 mt-4 font-bold">{river.name}</div>
-        <div className="mx-2 mt-1">Gen # {river.gen}</div>
-      </Link>
-    </div>
-  )
+  if (riverDetail)
+    return (
+      <div className="m-8 border bg-white p-2 text-left">
+        <Link href={`/river/${riverDetail.id}`}>
+          <Image className="mx-auto" src={StewardshipTokenSrc} alt="" width={200} height={200} />
+          <div className="mx-2 mb-1 mt-4 font-bold">{riverDetail.name}</div>
+          <div className="mx-2 mt-1">Gen # {river.generation}</div>
+        </Link>
+      </div>
+    )
 }
 export default StewardshipToken
